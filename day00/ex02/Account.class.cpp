a4737f69941781753a19354fe82cfcf074c4ee01
ryan <rleinbox@gmail.com>
2018-01-09 13:49:09 -0800

    fixed: typo

--- day00/ex02/Account.class.cpp
+++ day00/ex02/Account.class.cpp
@@ -102,7 +102,7 @@ void	Account::displayStatus( void ) const {
   std::cout << "index:" << this->_accountIndex
             << ";amount:" << this->_amount
             << ";deposits:" << this->_nbDeposits
-            << ";withdrawls:" << this->_nbWithdrawals
+            << ";withdrawals:" << this->_nbWithdrawals
             << std::endl;
 }
 
